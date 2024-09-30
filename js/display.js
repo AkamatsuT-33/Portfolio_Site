822f469111b54d633e7b9b26a15309581b4997e7
Tomoharu AKAMATSU <106578927+AkamatsuT-33@users.noreply.github.com>
2024-09-30 23:11:37 +0900

    index.htmlの作成

---
+++ js/display.js
@@ -0,0 +1,5 @@
+const title = document.getElementById("title");
+title.textContent = "Haru's PortFolio Sites.";
+
+const enter_web_site_btn = document.getElementById("enter_web_site");
+enter_web_site_btn.textContent = "Enter";
