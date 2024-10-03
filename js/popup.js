c2d33b4e510480d4243d211ce745652a1e3d153b
Tomoharu AKAMATSU <106578927+AkamatsuT-33@users.noreply.github.com>
2024-10-03 23:14:09 +0900

    メインページのワイヤーフレームの完成とフォルダ構造の変更

---
+++ js/popup.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const project_01 = document.getElementById('project_01');
+    const project_02 = document.getElementById('project_02');
+    const project_03 = document.getElementById('project_03');
+    const project_04 = document.getElementById('project_04');
+    const project_05 = document.getElementById('project_05');
+    const project_06 = document.getElementById('project_06');
+    
+    project_01.addEventListener('click', function() {
+      alert("project_01");
+    });
+
+    project_02.addEventListener('click', function() {
+        alert("project_02");
+    });
+    
+    project_03.addEventListener('click', function() {
+        alert("project_03");
+    });
+
+    project_04.addEventListener('click', function() {
+        alert("project_04");
+    });
+
+    project_05.addEventListener('click', function() {
+        alert("project_05");
+    });
+
+    project_06.addEventListener('click', function() {
+        alert("project_06");
+    });
+});
---
+++ js/text_display.js
@@ -0,0 +1,20 @@
+const tag_title = document.getElementById("tag_title");
+tag_title.textContent = "Haru's PortFolio Sites.";
+
+const profile_title = document.querySelector(".box_title_01");
+profile_title.textContent = "Profile";
+
+const name_01 = document.querySelector(".name_01");
+name_01.textContent = "Tomoharu AKAMATSU";
+
+const text_01 = document.querySelector(".text_01");
+text_01.textContent = "東京国際工科専門職大学 情報工学科 AI戦略コース 2024年卒 1期生\n";
+
+const projects_title = document.querySelector(".box_title_02");
+projects_title.textContent = "Projects";
+
+const skills_title = document.querySelector(".box_title_03");
+skills_title.textContent = "Skills";
+
+const future_title = document.querySelector(".box_title_04");
+future_title.textContent = "Future";
