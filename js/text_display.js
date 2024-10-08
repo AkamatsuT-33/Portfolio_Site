cd96476f82ec52128046c29d2525355569e27867
Tomoharu AKAMATSU <106578927+AkamatsuT-33@users.noreply.github.com>
2024-10-08 23:49:46 +0900

    画像の追加とデザインの継続

--- js/text_display.js
+++ js/text_display.js
@@ -1,20 +1,72 @@
 const tag_title = document.getElementById("tag_title");
 tag_title.textContent = "Haru's PortFolio Sites.";
 
-const profile_title = document.querySelector(".box_title_01");
+// 一段目タイトル
+const profile_title = document.getElementById("title_01");
 profile_title.textContent = "Profile";
 
 const name_01 = document.querySelector(".name_01");
 name_01.textContent = "Tomoharu AKAMATSU";
 
-const text_01 = document.querySelector(".text_02");
-text_01.textContent = "東京国際工科専門職大学 情報工学科 AI戦略コース 2024年卒 1期生\n";
+const text_01 = document.querySelector(".text_01");
+text_01.textContent = "東京国際工科専門職大学 情報工学科 AI戦略コース 2024年卒 1期生";
 
-const projects_title = document.querySelector(".box_title_02");
+const text_02 = document.querySelector(".text_02");
+text_02.textContent = '\
+                        大学では深層学習などのAI関連を学習してきました。\
+                        また、インターンとして、AI専門メディアでの記事執筆経験やAI開発・データ分析を行う会社での開発業務を経験しています。\
+                        AIと人が協力し合って社会課題を解決できるシステムを開発したり、社会に求められているシステムを実現するエンジニアになりたいと思っています。\
+                        ';
+
+// 二段目タイトル
+const projects_title = document.getElementById("title_02");
 projects_title.textContent = "Projects";
 
-const skills_title = document.querySelector(".box_title_03");
+const box_title_01 = document.getElementById("box_title_01");
+box_title_01.textContent = "転移学習を用いた胸部Ｘ線画像モデルの作成と性能評価";
+
+const box_text_01 = document.getElementById("box_text_01");
+box_text_01.innerHTML = "-KeyWord-<br>#AI #Python3";
+
+const box_title_02 = document.getElementById("box_title_02");
+box_title_02.textContent = "胸部X線画像分類デスクトップアプリ";
+
+const box_text_02 = document.getElementById("box_text_02");
+box_text_02.innerHTML = "-KeyWord-<br>#AI #Python3 #Electron #Node.js #Flask #PyTorch #TorchVision";
+
+
+const box_title_03 = document.getElementById("box_title_03");
+box_title_03.textContent = "数字当てゲームデスクトップアプリ";
+
+const box_text_03 = document.getElementById("box_text_03");
+box_text_03.innerHTML = "-KeyWord-<br>#Python3 #Tkinter #SQLight3";
+
+
+const box_title_04 = document.getElementById("box_title_04");
+box_title_04.textContent = "数字あてゲーム（コマンドライン）";
+
+const box_text_04 = document.getElementById("box_text_04");
+box_text_04.innerHTML = "-KeyWord-<br>#Python3";
+
+
+const box_title_05 = document.getElementById("box_title_05");
+box_title_05.textContent = "QRコードデスクトップアプリ";
+
+const box_text_05 = document.getElementById("box_text_05");
+box_text_05.innerHTML = "-KeyWord-<br>#Python3 #Tkinter #PIL #QRcode";
+
+
+const box_title_06 = document.getElementById("box_title_06");
+box_title_06.textContent = "ポートフォリオサイト";
+
+const box_text_06 = document.getElementById("box_text_06");
+box_text_06.innerHTML = "-KeyWord-<br>#HTML #CSS #JavaScript #BootStrap5";
+
+
+// 三段目タイトル
+const skills_title = document.getElementById("title_03");
 skills_title.textContent = "Skills";
 
-const future_title = document.querySelector(".box_title_04");
+// 四段目タイトル
+const future_title = document.getElementById("title_04");
 future_title.textContent = "Future";
