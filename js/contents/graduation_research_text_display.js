9df320461780c4ecc9ad49766544c55de60dc549
Tomoharu AKAMATSU <106578927+AkamatsuT-33@users.noreply.github.com>
2024-12-06 23:17:03 +0900

    軽微な修正

--- js/contents/graduation_research_text_display.js
+++ js/contents/graduation_research_text_display.js
@@ -4,8 +4,8 @@ research_title.innerHTML = "Graduation Research";
 
 const research_text = document.getElementById("research_text");
 research_text.innerHTML = `
-                            <h4 style="text-align:center">卒業論文</h4><br>
-                            <h5 style="text-align:center">Small World Neural Network（SWNN）を用いた脳神経回路シミュレーションと<br>小規模AIモデルの最適化の基礎研究</h5><br>
+                            <h4 style="text-align:center;">卒業論文</h4><br>
+                            <h5 style="text-align:center;">Small World Neural Network（SWNN）を用いた脳神経回路シミュレーションと<br>小規模AIモデルの最適化の基礎研究</h5><br>
                             
                             人工知能モデルの最適化のために、スモールワールド性という任意のノードにたどり着くために、
                             少ないノード数を経由するだけでよいことを用いたニューラルネットワークで、脳神経回路モデルをプログラム上に構築し、
